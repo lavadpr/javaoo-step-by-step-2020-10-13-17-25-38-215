c375de3435c231b8cf39c77e39003d74185f7927
LAVADPR <lavadpr@oocl.com>
2020-10-14 21:44:41 +0800

    [OO Step By Step] [Prince] feature: Practice 9 done

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,43 @@
 package practice09;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> members = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        if(members!=null){
+            for(Student member: members){
+                if(leader.getName().equals(member.getName()))
+                    this.leader = leader;
+            }
+        }
+        if(this.leader==null)
+            System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student member) {
+        if(member.klass.getNumber() == number)
+            members.add(member);
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,17 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        return klass.getLeader() == null ? super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + "." : super.introduce() + " I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
 }
