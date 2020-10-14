10cb5a7ab2cace45219a34ae931e7d8a0e292f5b
LAVADPR <lavadpr@oocl.com>
2020-10-14 19:54:35 +0800

    [OO Step By Step] [Prince] feature: Practice 8 done

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,28 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    Klass klass;
+    final String teacherIntroduction = " I am a Teacher. I ";
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce(){
+        return klass != null && klass.getNumber() != 0 ? super.introduce() + teacherIntroduction + "teach Class " + klass.getNumber() + "." : super.introduce() + teacherIntroduction + "teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student) {
+        return student.getKlass().getNumber() == klass.getNumber() ? super.introduce() + teacherIntroduction + "teach " + student.getName() + "." : super.introduce() + teacherIntroduction + "don't teach " + student.getName() + "." ;
+    }
 }
