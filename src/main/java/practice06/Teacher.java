d24a1f2360424144de5de51e5cab99d0cc069742
LAVADPR <lavadpr@oocl.com>
2020-10-14 16:34:06 +0800

    [OO Step By Step] [Prince] feature: Practice 6 done

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,21 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        return klass != 0 ? super.introduce() + " I am a Teacher. I teach Class " + klass + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+    public int getKlass() {
+        return klass;
+    }
 }
