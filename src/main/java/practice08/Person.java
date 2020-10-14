be9c99d529ab4e8107cef40dc0b6e989687a970e
LAVADPR <lavadpr@oocl.com>
2020-10-14 21:44:04 +0800

    [OO Step By Step] [Prince] fix: Practice 8 fix

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,5 +1,7 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String name;
@@ -25,5 +27,20 @@ public class Person {
 
     public String introduce(){
         return "My name is " + getName() + ". I am " + getAge() + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 }
